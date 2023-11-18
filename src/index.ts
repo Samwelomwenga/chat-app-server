f24e6ac9da6b0d824728d38cffc26a6992ab28a1
Samwel Omwenga <samwel.omwenga1@students.jkuat.ac.ke>
2023-11-18 21:18:46 +0300

    feat: Add the project files

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+
+console.log("Hello World sam");
+
+function multiply(a: number, b: number): number {
+    const result = a * b;
+    console.log(`The result of multiplying ${a} and ${b} is ${result}`);
+    return result;
+}
+
+multiply(2, 9);
